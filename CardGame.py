5ce71f744b8cf8593e49503fbd34e1a1b5d598ea
Joeyq101 <Joannawallace@hotmail.com>
2022-07-05 12:08:18 +1000

    Create CardGame.py

---
+++ CardGame.py
@@ -0,0 +1,10 @@
+# You are required to create a application which is a simple game.  The game will function as follows:
+
+#     Program will generate a random integer between 1 and 10
+#     Program will ask the user to guess the number
+#         If correct, congratulate the player, print how many guesses were made and exit
+#         If incorrect, prompt the user to guess again
+#     Repeat until user guesses the number
+
+# You will need to create a algorithm (flowchart or pseudocode) as well as creating the code.
+
